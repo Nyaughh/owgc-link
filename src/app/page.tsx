da21e7643aa92003fd4653b61538dc2c138dcf7f
Nyaughh <aarinarora02@gmail.com>
2024-04-27 12:38:45 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { WavyBackground } from "@/components/ui/wavy-background";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex items-center justify-center flex-grow mb-8"> {/* Added flex-grow and flex properties */}
+
+    <main className="flex h-screen w-full bg-black bg-grid-white/[0.2] relative items-center justify-centermin-h-screen flex-col justify-between">
+      <div className="relative flex items-center justify-center flex-grow space-y-2"> {/* Added flex-grow and flex properties */}
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/overwatch.svg"
