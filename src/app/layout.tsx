e2586a1ff92cd1cbb1476d52c20d7cbb98d713f1
Nyaughh <aarinarora02@gmail.com>
2024-04-27 11:53:32 +0530

    jhvbjhg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head"; // Import Head component from next/head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Overwatch WhatsApp Group Chat",
   description: "Join Now",
 };
@@ -17,15 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* Include the Head component and set metadata */}
-      <Head>
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        {/* Loop over images to include multiple images */}
-        {metadata.openGraph.images.map((image, index) => (
-          <meta property="og:image" key={index} content={image.url} />
-        ))}
-      </Head>
+      <head>
+        <meta property="og:image" content="/image.png"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
