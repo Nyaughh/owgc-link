8f723a1317b587dae18d971e6aa0edc5fc82db9b
AlenVelocity <alenyohannan71@gmail.com>
2024-04-27 13:20:02 +0530

    Update Prettier configuration and format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Overwatch WhatsApp Group Chat",
-  description: "Join Now",
-};
+    title: 'Overwatch WhatsApp Group Chat',
+    description: 'Join Now'
+}
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <meta property="og:image" content="/image.png"/>
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta property="og:image" content="/image.png" />
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
