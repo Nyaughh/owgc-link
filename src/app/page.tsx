4d09d96188246e04f58e4c3088617a819310b179
Nyaughh <aarinarora02@gmail.com>
2024-06-12 03:25:23 +0530

    dvh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Main from './Main'
 
 export default function Home() {
     return (
-        <main className="w-screen h-screen overflow-hidden">
+        <main className="w-screen h-[100dvh] overflow-hidden">
             <div className="p-4 m-2 box border border-blue-400 flex flex-col items-start mx-auto relative max-h-[98%] max-w-[95%] md:max-w-[99%] h-full overflow-hidden">
                 <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
                 <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
