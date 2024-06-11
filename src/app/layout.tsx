5c14394d75508620af30d086f44f331ab1dc38c1
Nyaughh <aarinarora02@gmail.com>
2024-06-12 03:00:04 +0530

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'Overwatch WhatsApp Group Chat',
+    title: 'Overwatch Community Links',
     description: 'Join Now'
 }
 
